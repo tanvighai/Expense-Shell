c4d7b0cd5264f12a5c754a417f25ed2a22b6da14
tanvi <tanvighai02@gmail.com>
2023-09-24 09:37:20 -0400

    task2 edited

--- Frontend.sh
+++ Frontend.sh
@@ -5,5 +5,5 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp frontend.conf /etc/nginx/default.d/expense.conf
+cp frontend.conf /etc/nginx/default.d/frontend.conf
 systemctl restart nginx
