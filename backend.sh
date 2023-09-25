d4902113940ce9b18b3254156a20c341a107924c
tanvi <tanvighai02@gmail.com>
2023-09-24 21:49:29 -0400

    complete shell scripts first commit

---
+++ backend.sh
@@ -0,0 +1,17 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+dnf install nodejs -y
+
+cp backend.service /etc/systemd/system/backend.service
+
+useradd expense
+mkdir /app
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+cd /app
+unzip /tmp/backend.zip
+cd /app
+npm install
+systemctl daemon-reload
+systemctl enable backend
+systemctl start backend
+dnf install mysql -y
+mysql -h m1.tanvi12online.net -uroot -pExpenseApp@1 < /app/schema/backend.sql
