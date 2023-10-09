3f0ac3ec547b67557b7ce71ee7bd33b5d909e75d
tanvi <tanvighai02@gmail.com>
2023-10-08 21:06:42 -0400

    checking my first function created

--- common.sh
+++ common.sh
@@ -3,8 +3,10 @@ log_file=/tmp/expense.log
 download_and_extract() {
   echo download $component code
   curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >>$log_file
-
+echo$?
 
   echo extracting Frontend code
   unzip /tmp/$component.zip >>$log_file
+
+  echo$?
 }
