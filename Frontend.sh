04e5d869af70ec2a2a13432479569de231204a22
tanvi <tanvighai02@gmail.com>
2023-10-08 20:48:42 -0400

    checking my first function created

--- Frontend.sh
+++ Frontend.sh
@@ -3,18 +3,18 @@ component=frontend
 
 
 echo installing Nginx
-dnf install nginx -y >>$log_file
+dnf install nginx -y &>>$log_file
 
 echo placing expense confif file in Ngnix
-cp expense.conf /etc/nginx/default.d/expense.conf >>$log_file
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 
 
 
 echo removing old Nginx content
-rm -rf /usr/share/nginx/html/* >>$log_file
+rm -rf /usr/share/nginx/html/* &>>$log_file
 
 cd /usr/share/nginx/html
 download_and_extract
 echo starting Ngnix service
-systemctl enable nginx >>$log_file
-systemctl restart nginx >>$log_file
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
--- backend.sh
+++ backend.sh
@@ -2,35 +2,35 @@ log_file=/tmp/expense.log
 
 component=backend
 echo install NodeJS repos
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash >>$log_file
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 
 echo install NodeJS
-dnf install nodejs -y >>$log_file
+dnf install nodejs -y &>>$log_file
 
 echo copy Backend service file
-cp backend.service /etc/systemd/system/backend.service >>$log_file
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
 
 echo add application user
-useradd expense >>$log_file
+useradd expense &>>$log_file
 
 echo clean app content
-rm-rf /app >>$log_file
+rm-rf /app &>>$log_file
 mkdir /app
 cd /app
 
 download_and_extract
 
 echo download dependencies
-npm install >>$log_file
+npm install &>>$log_file
 
 echo start backend service
-systemctl daemon-reload >>$log_file
-systemctl enable backend >>$log_file
-systemctl start backend >>$log_file
+systemctl daemon-reload &>>$log_file
+systemctl enable backend &>>$log_file
+systemctl start backend &>>$log_file
 
 echo install mysql client
-dnf install mysql -y >>$log_file
+dnf install mysql -y &>>$log_file
 
 echo load schema
-mysql -h m1.tanvi12online.net -uroot -pExpenseApp@1 < /app/schema/backend.sql >>$log_file
+mysql -h m1.tanvi12online.net -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
