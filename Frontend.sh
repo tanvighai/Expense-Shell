67a51e787052eaf016d59592022451d307403f4f
tanvi <tanvighai02@gmail.com>
2023-10-08 21:16:10 -0400

    checking my first function created

--- Frontend.sh
+++ Frontend.sh
@@ -4,20 +4,20 @@ component=frontend
 
 echo installing Nginx
 dnf install nginx -y &>>$log_file
-echo$?
+echo $?
 echo placing expense confif file in Ngnix
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
-echo$?
+echo $?
 
 
 echo removing old Nginx content
 rm -rf /usr/share/nginx/html/* &>>$log_file
-echo$?
+echo $?
 cd /usr/share/nginx/html
 download_and_extract
-echo$?
+echo $?
 echo starting Ngnix service
 systemctl enable nginx &>>$log_file
-echo$?
+echo $?
 systemctl restart nginx &>>$log_file
-echo$?
+echo $?
--- backend.sh
+++ backend.sh
@@ -3,16 +3,18 @@ log_file=/tmp/expense.log
 component=backend
 echo install NodeJS repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
+echo $?
 
 echo install NodeJS
 dnf install nodejs -y &>>$log_file
-
+echo $?
 echo copy Backend service file
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-
+echo $?
 
 echo add application user
 useradd expense &>>$log_file
+echo $?
 
 echo clean app content
 rm-rf /app &>>$log_file
@@ -20,17 +22,24 @@ mkdir /app
 cd /app
 
 download_and_extract
+echo $?
 
 echo download dependencies
 npm install &>>$log_file
+echo $?
+
 
 echo start backend service
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl start backend &>>$log_file
 
+echo $?
+
 echo install mysql client
 dnf install mysql -y &>>$log_file
+echo $?
 
 echo load schema
 mysql -h m1.tanvi12online.net -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
+echo $?
