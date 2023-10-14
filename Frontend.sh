bc43766474034e14d8426972f593201f1f5041d6
tanvi <tanvighai02@gmail.com>
2023-10-14 07:45:32 -0400

    checking my first function created

--- Frontend.sh
+++ Frontend.sh
@@ -4,20 +4,46 @@ component=frontend
 
 echo installing Nginx
 dnf install nginx -y &>>$log_file
-echo $?
+
+
+if [$? -eq 0];then
+echo SUCCESS
+else
+  echo FAIL
+fi
 echo placing expense confif file in Ngnix
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
-echo $?
+if [$? -eq 0];then
+echo SUCCESS
+else
+  echo FAIL
+fi
 
 
 echo removing old Nginx content
 rm -rf /usr/share/nginx/html/* &>>$log_file
-echo $?
+if [$? -eq 0];then
+echo SUCCESS
+else
+  echo FAIL
+fi
 cd /usr/share/nginx/html
 download_and_extract
-echo $?
+if [$? -eq 0];then
+echo SUCCESS
+else
+  echo FAILED
+fi
 echo starting Ngnix service
 systemctl enable nginx &>>$log_file
-echo $?
+if [$? -eq 0];then
+echo SUCCESS
+else
+  echo FAIL
+fi
 systemctl restart nginx &>>$log_file
-echo $?
+if [$? -eq 0];then
+echo SUCCESS
+else
+  echo FAIL
+fi
